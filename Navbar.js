708f4676a83ef1354599e3f07da103dfb2e25cfd
PALAKBA ZALA <24162122003@gnu.ac.in>
2026-02-22 23:47:06 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+    
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
