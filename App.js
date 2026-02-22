4a54a3b8f9c425363a86db7b68ddd651166ab2c2
PALAKBA ZALA <24162122003@gnu.ac.in>
2026-02-22 23:49:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";   
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
